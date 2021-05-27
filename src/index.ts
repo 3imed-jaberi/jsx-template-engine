d5a0f3abffaafcd2434dea4d4af579665e0dc8f9
3imed-jaberi <imed_jebari@hotmail.fr>
2021-05-28 01:29:32 +0200

    add importComponent + clenaup 🐱‍💻 ..

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,13 @@
  * Module dependencies.
  */
 import vm from "vm"
+import { promisify } from "util"
 import jsx, { Component, Props } from "./jsx"
-import { readFileSync as fsReadFileSync } from "fs"
 import { transform, transformSync } from "@babel/core"
-import { join as pathJoin, dirname as pathDirname, resolve as pathResolve } from "path"
+import { readFile, readFileSync as fsReadFileSync, existsSync as fsExistsSync } from "fs"
+import { join as pathJoin, dirname as pathDirname, extname as pathExtname } from "path"
 
+const fsReadFile = promisify(readFile)
 /**
  * Constants.
  *
@@ -33,6 +35,7 @@ const BABEL_OPTIONS = {
     ]
   ]
 }
+const SUPPORTED_EXTENSIONS = ["jsx", "js", ""]
 
 /**
  * a helper fuction to check any value is function.
@@ -44,6 +47,26 @@ function isFunction (value: unknown) {
 }
 
 /**
+ * a helper fuction to find file.
+ *
+ * @api private
+ */
+function fsFindFile(path: string): string {
+  const fileExtension = pathExtname(path).slice(1)
+  
+  if (!SUPPORTED_EXTENSIONS.includes(fileExtension)) {
+    throw new Error('please use .jsx or .js as extension for your file.')
+  }
+  
+  for (const extension of SUPPORTED_EXTENSIONS) {
+    const filePath = `${path}.${extension}`
+    if (fsExistsSync(filePath)) return filePath
+  }
+
+  return path
+}
+
+/**
  * require handler used with the vm.
  *
  * @api private
@@ -52,7 +75,7 @@ const vmRequire = (rootPath: string): any => (filePath: string): any => {
   // merge all paths to one.
   const path = pathJoin(rootPath, filePath)
   // read the code file.
-  const code = importComponent(path)
+  const code = fsReadFileSync(fsFindFile(path)).toString()
   // transformed code with babel.
   const babelCode = transformSync(code, BABEL_OPTIONS)?.code
   // use the vm module to execute the babelcode.
@@ -86,7 +109,7 @@ function vmRuntime(code: string, path?: string) {
  *
  * @api public
  */
-async function jsxEngine(code: string, payload: Props = {}, path?: string): Promise<string> {
+ export async function jsxEngine(code: string, payload: Props = {}, path?: string): Promise<string> {
     // execute the babel transform to compile the passed code.
     const babelCode = (await transform(code, BABEL_OPTIONS))?.code
 
@@ -104,6 +127,11 @@ async function jsxEngine(code: string, payload: Props = {}, path?: string): Prom
         )
     )
 
+    // make sure we have a function component before render.
+    if (!func) {
+      throw new Error("JSX file must return a function")
+    }
+
     // use our custom pragma render method.
     const html = jsx.render(func(payload))
 
@@ -111,21 +139,18 @@ async function jsxEngine(code: string, payload: Props = {}, path?: string): Prom
 }
 
 /**
- * a jsx component (file) reader. 
+ * a jsxEngine wrapper provide an implicit file reader.
  *
  * @api public
  */
-function importComponent(path: string) {
-  // resolve the dot-env file path.
-  const filePath = pathResolve(process.cwd(), path)
-  console.log(filePath);
-  
-  // extract the file content.
-  const fileContent = fsReadFileSync(filePath, { encoding: "utf8" }).toString()
-
-  // return the content
-  return fileContent
+export async function renderFile(path: string, payload: Props = {}): Promise<string> {
+  const code = (await fsReadFile(fsFindFile(path))).toString()
+  return jsxEngine(code, payload, path)
 }
 
-export default { render: jsxEngine, jsxEngine, importComponent }
-module.exports = { render: jsxEngine, jsxEngine, importComponent }
+export default { render: jsxEngine, renderFile }
+
+module.exports = { render: jsxEngine }
+module.exports.default = { render: jsxEngine }
+module.exports.jsxEngine = jsxEngine
+module.exports.renderFile = renderFile
